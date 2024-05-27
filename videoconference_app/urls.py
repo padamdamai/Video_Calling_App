9785e8950fd1626a40b2b2dd22679cb8b7cc1a98
Padam Damai <padamdamai888@gmail.com>
2024-05-28 00:06:00 +0545

    login initial

--- videoconference_app/urls.py
+++ videoconference_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('register/',views.register,name='register'),
-    path('login/',views.login,name='login'),
+    path('login/',views.login_view,name='login'),
+    path('dashboard',views.dashboard,name='dashboard'),
 
 ]
--- videoconference_app/views.py
+++ videoconference_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
 def register(request):
@@ -13,5 +15,20 @@ def register(request):
 
     return render(request, 'register.html')
 
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request,email = email ,password = password)
+        if user is not None: #if there is user in database 
+            login(request,user)
+            return render("/dashboard")
+        else:
+            return render(request,'login.html',{'error': 'invalid user ,please try again'})
+    
     return render(request,'login.html')
+
+@login_required
+def dashboard(request):
+    
+    return render(request,'dashboard.html')
